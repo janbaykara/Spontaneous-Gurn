dedebfeedb4ecba36b03800d2ad03c5029365d82
Jan Baykara <janbaykara@gmail.com>
2014-10-11 15:56:00 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+# 'Spontaneous Node'
+
+### A personal MEAN boilerplate
+    
+  This is where I merge up all my frontend solutions from various work/personal projects into one foundational template. When I find a nifty way of structuring/doing stuff, I'll dump it here so I can reuse it for next time.
+
+  I may or may not turn it into a yeoman generator at some point.
+    
+### Frontend 7th Symphony, composed by Wolfgang Gulpenstein
+  - A meaty **gulpfile** that works alongside **skeleton structures** for...
+  - **OOSCSS**
+    - *SASS* preprocessor
+    - *Normalize.css* by (@necolas v3.0.2)
+    - *clrs.cc* (@mrmrs v2.0)
+  - **AngularJS** & **Angular UI Router**
+
+  NB: All outdoor-facing frontend assets distribute to `/assets/(styles|scripts|images|...)`
+
+### TODO: Backend Overture, by the massed phillharmonic Node/Sails orchestra
+  - An optional skeleton for **Node.js** development utilising...
+  - **Sails.js** 
+  - **Swig** html rendering
+  - **Passport.js** auth
+  - **Orchestrate.io** data service (via sails-orchestra adapter)
---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+// Dependencies
+var gulp = require('gulp')
+  , gulpLoadPlugins = require('gulp-load-plugins')
+  , mainBowerFiles = require('main-bower-files')
+  , browserSync = require('browser-sync')
+  , browserReload = browserSync.reload
+
+// Autoload Gulp tasks from package.json
+var plugins = gulpLoadPlugins();
+
+// Paths
+var dirs = {
+  dev: {
+    config:  ['bower.json','package.json'],
+    img:     ['frontend/img/**/*.jpg',
+              'frontend/img/**/*.jpeg',
+              'frontend/img/**/*.gif',
+              'frontend/img/**/*.png'],
+    svg:     ['frontend/img/**/*.svg'],
+    js:      ['frontend/js/*.js'],
+    data:    ['frontend/data/*.json'],
+    sass:    ['frontend/sass/*.scss'],
+    fonts:   ['frontend/fonts/*'],
+    html:    ['frontend/html/*']
+  },
+  prod: {
+    images:   'assets/images',
+    scripts:  'assets/scripts',
+    styles:   'assets/styles',
+    views:    'assets/views'
+  }
+}
+
+gulp.task('install', function () {
+  return gulp.src(dirs.dev.config)
+  .pipe(plugins.install());
+});
+
+// ----------------------------------------------------------------
+// Styles
+
+  gulp.task('styles', function () {
+    gulp.src(dirs.dev.sass)
+    .pipe(plugins.sass({
+        errLogToConsole: true
+    }))
+    .pipe(plugins.autoprefixer("last 1 version", "> 1%", "ie 8", "ie 7"))
+    .pipe(plugins.size({showFiles: true}))
+    .pipe(plugins.minifyCss())
+    // .pipe(plugins.csslint({
+    //     'compatible-vendor-prefixes': false,
+    //     'box-sizing': false,
+    //     'important': false,
+    //     'known-properties': false
+    // }))
+    // .pipe(plugins.csslint.reporter())
+    .pipe(plugins.rename({suffix: '.min'}))
+    .pipe(plugins.size({showFiles: true}))
+    .pipe(gulp.dest(dirs.prod.styles))
+    .pipe(browserSync.reload({stream:true}));
+  });
+
+  gulp.task('fonts', function() {
+    gulp.src(dirs.dev.fonts)
+    .pipe(gulp.dest(dirs.prod.styles));
+  });
+
+// ----------------------------------------------------------------
+// Javascript
+
+  // Vendor JS
+  gulp.task('libs', function() {
+    gulp.src(mainBowerFiles(/* options */), { base: 'vendor' })
+    .pipe(plugins.concat('libs.min.js'))
+    .pipe(plugins.size({showFiles: true}))
+    .pipe(plugins.uglify({mangle: false}))
+    .pipe(plugins.size({showFiles: true}))
+    .pipe(gulp.dest(dirs.prod.scripts));
+  });
+
+  // Project JS
+  gulp.task('scripts', function() {
+    gulp.src(dirs.dev.js)
+    .pipe(plugins.concat('app.min.js'))
+    .pipe(plugins.size({showFiles: true}))
+    .pipe(plugins.uglify({mangle: false}))
+    .pipe(plugins.size({showFiles: true}))
+    .pipe(gulp.dest(dirs.prod.scripts));
+  });
+
+  // Datafiles
+  gulp.task('data', function() {
+    gulp.src(dirs.dev.data)
+    .pipe(gulp.dest(dirs.prod.scripts));
+  });
+
+  // Datafiles
+  gulp.task('views', function() {
+    gulp.src(dirs.dev.html)
+    .pipe(gulp.dest(dirs.prod.views));
+  });
+
+// ----------------------------------------------------------------
+// Images
+
+    gulp.task('rasters', function() {
+      gulp.src(dirs.dev.img)
+      .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+      .pipe(gulp.dest(dirs.prod.images));
+    });
+
+    gulp.task('vectors', function() {
+      gulp.src(dirs.dev.svg)
+      .pipe(plugins.svgmin())
+      .pipe(plugins.replace(/_[0-9]+_/g, '')) // Illustrator SVGs; strip appended strings from id names
+      .pipe(gulp.dest(dirs.prod.images));
+    });
+
+// ----------------------------------------------------------------
+// Start server & reload on CSS changes
+
+    gulp.task('browser-sync', function() {
+        browserSync.init(null, { server: { baseDir: "./" } });
+    });
+
+    // Function to call for reloading browsers
+    gulp.task('bs-reload', function () {
+        browserSync.reload();
+    });
+
+// ----------------------------------------------------------------
+// Tasks
+
+gulp.task('watch', function() {
+  gulp.watch(dirs.dev.js,     ['scripts']);
+  gulp.watch(dirs.dev.libs,   ['libs']);
+  gulp.watch(dirs.dev.data,   ['data']);
+  //
+  gulp.watch(dirs.dev.sass,   ['styles']);
+  gulp.watch(dirs.prod.styles,['bs-reload']);
+  //
+  gulp.watch(dirs.dev.fonts,  ['fonts']);
+  gulp.watch(dirs.dev.img,    ['rasters']);
+  gulp.watch(dirs.dev.svg,    ['vectors']);
+  //
+  gulp.watch(dirs.dev.html,   ['views']);
+  gulp.watch(dirs.prod.views, ['bs-reload']);
+});
+
+gulp.task('build', ['rasters', 'vectors', 'styles', 'fonts', 'scripts', 'libs', 'views', 'data']);
+gulp.task('start', ['bs-reload']);
+gulp.task('init', ['install', 'build']);
+gulp.task('default', ['build', 'watch', 'start']);
